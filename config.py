3dd999201441c9435d797b329392775ef38bdda8
Noah Rosenbaum <noahrosenbaum762@gmail.com>
2021-08-06 10:54:42 -0500

    commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Gives access to the project in ANY OS we are in
+# Allows outside files/folders to be added to the project from the base directory
+
+class Config:
+    """
+    Sets configuration variables for our Flask app here
+    Eventuallu will use hidden variable items, but for now, we'll leave them exposed in config
+    """
+    SECRET_KEY = "You will never guess..."
+    DEPLOY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
